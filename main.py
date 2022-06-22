9e1cf84b11d22cb50b26560f90bbf9ee7492d93e
Aaditya Dhruv <aadityadhruv@mailbox.org>
2022-06-21 18:19:01 -0700

    basic key functions with cider api

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import os
 import argparse 
+import requests
+
+def previous():
+    requests.get("http://localhost:9000/api/playback/previous")
+def playpause():
+    requests.get("http://localhost:9000/api/playback/playpause")
+def next():
+    requests.get("http://localhost:9000/api/playback/next")
 def ampp_main():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-a', '--action', help='repo help', required=True)
     args = parser.parse_args()
+
+    if (args.action == "left"):
+        previous()
+    elif (args.action == "right"):
+        next()
+    elif (args.action == "play"):
+        playpause()
+    
 
 
 
